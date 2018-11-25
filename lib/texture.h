b2ce52c99d9c6618df76539850dd82fceb8772e3
Nicholas Blott <blottn@tcd.ie>
2018-11-25 15:31:59 +0000

    stars!

--- lib/texture.h
+++ lib/texture.h
@@ -10,7 +10,7 @@ GLuint loadCubemap(std::vector<std::string> faces)
     int width, height, nrChannels;
     for (int i = 0; i < faces.size(); i++)
     {
-        stbi_uc * data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        stbi_uc * data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 3);
         if (data)
         {
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
